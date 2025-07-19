249e3bf8b7c38a1a87823055b4a85b06f7fa05ca
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-07-19 17:36:31 -0400

    Implement basic user input to questions

--- app/src/main/java/org/example/TextUI.java
+++ app/src/main/java/org/example/TextUI.java
@@ -12,7 +12,42 @@ public class TextUI {
     }
 
     public void start() {
+        this.askMealCategory();
+        String mealCategory = this.receiveInput();
+        this.askMealName();
+        String mealName = this.receiveInput();
+        this.askIngredients();
+        String ingredients = this.receiveInput();
+        System.out.println();
+        this.printMeal(mealCategory, mealName, ingredients);
+        System.out.println("The meal has been added!");
+    }
 
+    public void askMealCategory() {
+        System.out.println("Which meal do you want to add " +
+                "(breakfast, lunch, dinner)?");
+    }
+
+    public String receiveInput() {
+        return this.scanner.nextLine();
+    }
+
+    public void askMealName() {
+        System.out.println("Input the meal's name:");
+    }
+
+    public void askIngredients() {
+        System.out.println("Input the ingredients:");
+    }
+
+    public void printMeal(String mealCategory, String mealName,
+                          String ingredients) {
+        System.out.println("Category " + mealCategory);
+        System.out.println("Name: " + mealName);
+        System.out.println("Ingredients:");
+        for (String word : ingredients.split(",")) {
+            System.out.println(word);
+        }
     }
 
 }
