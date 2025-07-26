79ae89d9a97ffbc53d8617780c6886c425fc4c80
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-07-26 08:15:21 -0400

    add ingredients to ingredients table

--- app/src/main/java/org/example/MealsDb.java
+++ app/src/main/java/org/example/MealsDb.java
@@ -30,24 +30,55 @@ public class MealsDb {
     void createMeal(String category, String meal, ArrayList<String> ingredients) {
         // add category and meal to meals table
         // get the meal_id
+        // add ingredients
 
-        String sql = "INSERT INTO meals (category, meal) VALUES (?, ?)";
+        int mealId = 0;
+
+        String sqlInsertMeal = "INSERT INTO meals (category, meal) VALUES (?, ?)";
         try (
                 Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                PreparedStatement pstmt = conn.prepareStatement(sql);
+                PreparedStatement pstmt = conn.prepareStatement(sqlInsertMeal, Statement.RETURN_GENERATED_KEYS);
         ) {
             pstmt.setString(1, category);
             pstmt.setString(2, meal);
 
             int rowsInserted = pstmt.executeUpdate();
-
             System.out.println(rowsInserted + " row(s) inserted.");
+
+            if (rowsInserted > 0) {
+                try (ResultSet rs = pstmt.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        mealId = rs.getInt("meal_id");
+                        System.out.println("Generated meal_id: " + mealId);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String sqlInsertIngredients = "INSERT INTO ingredients (ingredient, meal_id) VALUES (?, ?)";
+        try (
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                PreparedStatement pstmt = conn.prepareStatement(sqlInsertIngredients);
+        ) {
+            conn.setAutoCommit(false);
+
+            for (String item : ingredients) {
+                pstmt.setString(1, item);
+                pstmt.setInt(2, mealId);
+                pstmt.addBatch();
+            }
+
+            pstmt.executeBatch();
+            conn.commit();
+            System.out.println("Batch insert completed.");
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        // add all ingredients
+
     }
 
 }
